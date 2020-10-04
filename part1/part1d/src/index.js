0115886465e1c760e856b24509badc8134bbc13c
Michael Singh <singh.michaelanthony@gmail.com>
2020-10-04 10:33:10 -0400

    part1d debug react apps section

--- part1/part1d/src/index.js
+++ part1/part1d/src/index.js
@@ -1,17 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const History = props => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressig the buttons
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const Button = ({ onClick, text}) => <button onClick={onClick}>{text}</button>
+
+const App = props => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([]) // stores click history into an array
+
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L')) // concat doesn't mutate the existing array which is highly NOT recommended in React state hooks
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
+
+  return (
+    <div>
+      <div>
+        {left}
+        <Button onClick={handleLeftClick} text="left" />
+        <Button onClick={handleRightClick} text="right" />
+        {right}
+        <History allClicks={allClicks} />
+      </div>
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
